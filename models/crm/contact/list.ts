134175396c349f96e59dab196e7240b696e35fd2
Henri <HenriChabrand@users.noreply.github.com>
2024-02-12 13:26:35 +0100

    Update package version

--- models/crm/contact/list.ts
+++ models/crm/contact/list.ts
@@ -7,20 +7,20 @@ const metadata:Metadata<Crm.Contact> = {
 };
 
 export default new List( async (runtime, { filters }) => { 
-	
-	const response = await runtime.proxy({
-        method: 'GET',
-        path: '/crm/v3/objects/contacts'
-    });
 
-	const resources: Resource<Crm.Contact>[] = response.results.map((hs_contact) => (new Resource<Crm.Contact>({ 
-		id: hs_contact.id,
-		data: {
-			first_name: hs_contact.properties.firstname,
-			last_name: hs_contact.properties.lastname,
-			email: hs_contact.properties.email,
-			phone: hs_contact.properties.phone
-		},
+const response = await runtime.proxy({
+	method: 'GET',
+	path: '/crm/v3/objects/contacts'
+});
+
+const resources: Resource<Crm.Contact>[] = response.results.map((hs_contact) => (new Resource<Crm.Contact>({ 
+	id: hs_contact.id,
+	data: {
+		first_name: hs_contact.properties.firstname,
+		last_name: hs_contact.properties.lastname,
+		email: hs_contact.properties.email,
+		phone: hs_contact.properties.phone
+	},
 		created_at: new Date(hs_contact.createdAt).toISOString(),
 		updated_at: new Date(hs_contact.updatedAt).toISOString()
 	}, metadata.model)));  
