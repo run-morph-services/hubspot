7291d0a8351e1fb73a508feaf388b7ca5bdb36fc
Henri <HenriChabrand@users.noreply.github.com>
2024-02-20 16:35:49 +0100

    Added Generic User Get

---
+++ models/generic/user/retrieve.ts
@@ -0,0 +1,49 @@
+import { Generic }  from '@run-morph/models';
+import { Retrieve, Resource, Metadata, Error }  from '@run-morph/sdk';
+
+// Define metadata for the HubSpot pipeline model
+const metadata:Metadata<Generic.User> = {
+	model: Generic.User,
+	scopes: ['crm.objects.owners.read']
+};
+
+
+export default new Retrieve( async (runtime, { id }) => { 
+	console.log(id)
+	
+	// Call the HubSpot API to GET a user 
+	const response = await runtime.proxy({
+		method: 'GET',
+		path: `/crm/v3/owners/${id}`
+	});
+
+	
+	console.log(response)
+	// Handle errors from the API response
+	if(response.status === 'error'){
+        switch (response.category){
+            default:
+                throw new Error(Error.Type.UNKNOWN_ERROR, response.message);
+        }
+    }
+
+	// Map resource for the response
+	const resource = mapResource(response) 
+
+	return resource
+
+}, metadata );
+
+
+
+// Helper function to map HubSpot owenr to Generic.User resource
+function mapResource(hs_owner){
+	return new Resource({ 
+		id: hs_owner.id,
+		data: {
+			name: hs_owner.firstName+' '+hs_owner.lastName
+		},
+		created_at: new Date(hs_owner.createdAt).toISOString(),
+		updated_at: new Date(hs_owner.updatedAt).toISOString()
+	}, Generic.User)
+}
